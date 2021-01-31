a82698269e406a6a034e653cce2073b77f7af3a8
Bogdan Cristea <cristeab@gmail.com>
2021-01-31 19:28:25 +0200

    disable translations

--- build-mumble.bat
+++ build-mumble.bat
@@ -10,7 +10,7 @@ pushd build
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-cmake -G "Ninja" "-DVCPKG_TARGET_TRIPLET=x64-windows-static-md" "-Dstatic=ON" "-DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake" "-Dclient=ON" "-Dserver=OFF" "-Dzeroconf=OFF" "-Ddbus=OFF" "-Doverlay=OFF" "-Dplugins=OFF" "-Dpackaging=ON" "-DRELEASE_ID=%RELEASE_ID%" "-DBUILD_NUMBER=%BUILD_NUMBER%" "-DCMAKE_INSTALL_PREFIX=%INSTALL_PATH%" "-DCMAKE_BUILD_TYPE=Release" ..
+cmake -G "Ninja" "-DVCPKG_TARGET_TRIPLET=x64-windows-static-md" "-Dstatic=ON" "-DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake" "-Dclient=ON" "-Dserver=OFF" "-Dzeroconf=OFF" "-Ddbus=OFF" "-Doverlay=OFF" "-Dplugins=OFF" "-Dpackaging=ON" "-Dtranslations=OFF" "-DRELEASE_ID=%RELEASE_ID%" "-DBUILD_NUMBER=%BUILD_NUMBER%" "-DCMAKE_INSTALL_PREFIX=%INSTALL_PATH%" "-DCMAKE_BUILD_TYPE=Release" ..
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 cmake --build .
