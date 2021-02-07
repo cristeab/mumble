783dd06706bc2b570e9d7634fa2cc9a196d6e359
Bogdan Cristea <cristeab@gmail.com>
2021-02-07 14:49:15 +0200

    all qml files

--- src/mumble/main.cpp
+++ src/mumble/main.cpp
@@ -571,14 +571,14 @@ int main(int argc, char **argv) {
 
 	g.lcd = new LCD();
 
-	// Process any waiting events before initializing our MainWindow.
+    // Process any waiting events before initializing our M ainWindow.
 	// The mumble:// URL support for Mac OS X happens through AppleEvents,
 	// so we need to loop a little before we begin.
 	a.processEvents();
 
 	// Main Window
     QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
 
     //g.mw = new MainWindow(nullptr);
     //g.mw->show();
