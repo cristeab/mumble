04355ff942e2f93bb00d5510c4ec34a650af21de
Bogdan Cristea <cristeab@gmail.com>
2021-02-26 23:20:05 +0200

    refac

--- src/mumble/Settings.cpp
+++ src/mumble/Settings.cpp
@@ -705,8 +705,14 @@ void Settings::load(QSettings *settings_ptr) {
 
 	LOAD(qsDatabaseLocation, "databaselocation");
 
-	LOAD(bMute, "audio/mute");
-	LOAD(bDeaf, "audio/deaf");
+	SAVELOAD(bMute, "audio/mute");
+	SAVELOAD(bDeaf, "audio/deaf");
+
+    SAVELOAD(inputSystemIndex, "audio/inputSystemIndex");
+    SAVELOAD(inputDeviceIndex, "audio/inputDeviceIndex");
+    SAVELOAD(outputSystemIndex, "audio/outputSystemIndex");
+    SAVELOAD(outputDeviceIndex, "audio/outputDeviceIndex");
+
 	LOADENUM(atTransmit, "audio/transmit");
 	LOAD(uiDoublePush, "audio/doublepush");
 	LOAD(pttHold, "audio/ptthold");
@@ -1012,11 +1018,6 @@ void Settings::load(QSettings *settings_ptr) {
 	settings_ptr->beginGroup(QLatin1String("overlay"));
 	os.load(settings_ptr);
 	settings_ptr->endGroup();
-
-    SAVELOAD(inputSystemIndex, "audio/inputSystemIndex");
-    SAVELOAD(inputDeviceIndex, "audio/inputDeviceIndex");
-    SAVELOAD(outputSystemIndex, "audio/outputSystemIndex");
-    SAVELOAD(outputDeviceIndex, "audio/outputDeviceIndex");
 }
 
 #undef LOAD
@@ -1129,32 +1130,38 @@ void Settings::save() {
 
 	SAVE(qsDatabaseLocation, "databaselocation");
 
-	SAVE(bMute, "audio/mute");
-	SAVE(bDeaf, "audio/deaf");
-	SAVE(atTransmit, "audio/transmit");
-	SAVE(uiDoublePush, "audio/doublepush");
-	SAVE(pttHold, "audio/ptthold");
-	SAVE(bTxAudioCue, "audio/pushclick");
-	SAVE(qsTxAudioCueOn, "audio/pushclickon");
-	SAVE(qsTxAudioCueOff, "audio/pushclickoff");
-	SAVE(iQuality, "audio/quality");
-	SAVE(iMinLoudness, "audio/loudness");
-	SAVE(fVolume, "audio/volume");
-	SAVE(fOtherVolume, "audio/othervolume");
-	SAVE(bAttenuateOthers, "audio/attenuateothers");
-	SAVE(bAttenuateOthersOnTalk, "audio/attenuateothersontalk");
-	SAVE(bAttenuateUsersOnPrioritySpeak, "audio/attenuateusersonpriorityspeak");
-	SAVE(bOnlyAttenuateSameOutput, "audio/onlyattenuatesameoutput");
-	SAVE(bAttenuateLoopbacks, "audio/attenuateloopbacks");
-	SAVE(vsVAD, "audio/vadsource");
-	SAVE(fVADmin, "audio/vadmin");
-	SAVE(fVADmax, "audio/vadmax");
-	SAVE(noiseCancelMode, "audio/noiseCancelMode");
-	SAVE(iSpeexNoiseCancelStrength, "audio/speexNoiseCancelStrength");
-	SAVE(bAllowLowDelay, "audio/allowlowdelay");
-	SAVE(uiAudioInputChannelMask, "audio/inputchannelmask");
-	SAVE(iVoiceHold, "audio/voicehold");
-	SAVE(iOutputDelay, "audio/outputdelay");
+	SAVELOAD(bMute, "audio/mute");
+	SAVELOAD(bDeaf, "audio/deaf");
+
+    SAVELOAD(inputSystemIndex, "audio/inputSystemIndex");
+    SAVELOAD(inputDeviceIndex, "audio/inputDeviceIndex");
+    SAVELOAD(outputSystemIndex, "audio/outputSystemIndex");
+    SAVELOAD(outputDeviceIndex, "audio/outputDeviceIndex");
+
+	SAVELOAD(atTransmit, "audio/transmit");
+	SAVELOAD(uiDoublePush, "audio/doublepush");
+	SAVELOAD(pttHold, "audio/ptthold");
+	SAVELOAD(bTxAudioCue, "audio/pushclick");
+	SAVELOAD(qsTxAudioCueOn, "audio/pushclickon");
+	SAVELOAD(qsTxAudioCueOff, "audio/pushclickoff");
+	SAVELOAD(iQuality, "audio/quality");
+	SAVELOAD(iMinLoudness, "audio/loudness");
+	SAVELOAD(fVolume, "audio/volume");
+	SAVELOAD(fOtherVolume, "audio/othervolume");
+	SAVELOAD(bAttenuateOthers, "audio/attenuateothers");
+	SAVELOAD(bAttenuateOthersOnTalk, "audio/attenuateothersontalk");
+	SAVELOAD(bAttenuateUsersOnPrioritySpeak, "audio/attenuateusersonpriorityspeak");
+	SAVELOAD(bOnlyAttenuateSameOutput, "audio/onlyattenuatesameoutput");
+	SAVELOAD(bAttenuateLoopbacks, "audio/attenuateloopbacks");
+	SAVELOAD(vsVAD, "audio/vadsource");
+	SAVELOAD(fVADmin, "audio/vadmin");
+	SAVELOAD(fVADmax, "audio/vadmax");
+	SAVELOAD(noiseCancelMode, "audio/noiseCancelMode");
+	SAVELOAD(iSpeexNoiseCancelStrength, "audio/speexNoiseCancelStrength");
+	SAVELOAD(bAllowLowDelay, "audio/allowlowdelay");
+	SAVELOAD(uiAudioInputChannelMask, "audio/inputchannelmask");
+	SAVELOAD(iVoiceHold, "audio/voicehold");
+	SAVELOAD(iOutputDelay, "audio/outputdelay");
 
 	// Idle auto actions
 	SAVE(iIdleTime, "audio/idletime");
@@ -1393,11 +1400,6 @@ void Settings::save() {
 	settings_ptr->beginGroup(QLatin1String("overlay"));
 	os.save(settings_ptr);
 	settings_ptr->endGroup();
-
-    SAVELOAD(inputSystemIndex, "audio/inputSystemIndex");
-    SAVELOAD(inputDeviceIndex, "audio/inputDeviceIndex");
-    SAVELOAD(outputSystemIndex, "audio/outputSystemIndex");
-    SAVELOAD(outputDeviceIndex, "audio/outputDeviceIndex");
 }
 
 #undef LOAD
